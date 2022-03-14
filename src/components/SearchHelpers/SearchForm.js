489e985c61e4462b62175e2625b5f95da4a860f8
Amin <aminelnasri@hotmail.com>
2022-03-14 07:35:02 -0600

    Add additional styling to Search form

--- src/components/SearchHelpers/SearchForm.js
+++ src/components/SearchHelpers/SearchForm.js
@@ -1,154 +1,156 @@
 import * as React from 'react';
 import TextField from '@mui/material/TextField';
-import { Button, Grid } from "@mui/material";
+import { Button, Grid, Paper, Typography } from "@mui/material";
 import { TypeSelectBox } from '../Create Pokemon Components/TypeSelectBox';
 
 
 export const SearchForm = ({ type1, setType1, type2, setType2, handleChange, handleSubmit }) => {
   return (
+    <Paper sx={{ m: 1 }}>
+      <Typography align='center' component="h2" variant="h4" >Search for a Pokemon</Typography>
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        justifyContent="center">
+        <form onSubmit={handleSubmit}>
 
-    <Grid
-      container
-      direction="column"
-      alignItems="center"
-      justifyContent="center">
-      <form onSubmit={handleSubmit}>
-        
-        <div>
-          <TextField
-            label="Pokemon Name"
-            id="outlined-start-adornment"
-            sx={{ m: 1, width: '40ch' }}
-            onChange={handleChange('name')}
-          />
+          <div>
+            <TextField
+              label="Pokemon Name"
+              id="outlined-start-adornment"
+              sx={{ m: 1, width: '40ch' }}
+              onChange={handleChange('name')}
+            />
 
-          <TypeSelectBox typeNum={1} type={type1} setType={setType1} required={false} />
-          <TypeSelectBox typeNum={2} type={type2} setType={setType2} required={false} />
+            <TypeSelectBox typeNum={1} type={type1} setType={setType1} required={false} />
+            <TypeSelectBox typeNum={2} type={type2} setType={setType2} required={false} />
 
-        </div>
+          </div>
 
-        <div>
-          <TextField
-            label="Ability 1"
-            id="outlined-start-adornment"
-            sx={{ m: 1, width: '30ch' }}
-            onChange={handleChange('ability1')}
-          />
+          <div>
+            <TextField
+              label="Ability 1"
+              id="outlined-start-adornment"
+              sx={{ m: 1, width: '30ch' }}
+              onChange={handleChange('ability1')}
+            />
 
-          <TextField
-            label="Ability 2"
-            id="outlined-start-adornment"
-            sx={{ m: 1, width: '30ch' }}
-            onChange={handleChange('ability2')}
-          />
+            <TextField
+              label="Ability 2"
+              id="outlined-start-adornment"
+              sx={{ m: 1, width: '30ch' }}
+              onChange={handleChange('ability2')}
+            />
 
-          <TextField
-            label="HP Minimum"
-            type="number"
-            sx={{ m: 1, width: '30ch' }}
-            onChange={handleChange('hpLowerBound')}
+            <TextField
+              label="HP Minimum"
+              type="number"
+              sx={{ m: 1, width: '30ch' }}
+              onChange={handleChange('hpLowerBound')}
 
-          />
-          <TextField
-            label="HP Maximum"
-            type="number"
-            sx={{ m: 1, width: '30ch' }}
-            onChange={handleChange('hpUpperBound')}
-          />
-        </div>
+            />
+            <TextField
+              label="HP Maximum"
+              type="number"
+              sx={{ m: 1, width: '30ch' }}
+              onChange={handleChange('hpUpperBound')}
+            />
+          </div>
 
-        <div>
-          <TextField
-            label="Attack Minimum"
-            type="number"
-            sx={{ m: 1, width: '30ch' }}
-            onChange={handleChange('attackLowerBound')}
-          />
+          <div>
+            <TextField
+              label="Attack Minimum"
+              type="number"
+              sx={{ m: 1, width: '30ch' }}
+              onChange={handleChange('attackLowerBound')}
+            />
 
-          <TextField
-            label="Attack Maximum"
-            type="number"
-            sx={{ m: 1, width: '30ch' }}
-            onChange={handleChange('attackUpperBound')}
-          />
+            <TextField
+              label="Attack Maximum"
+              type="number"
+              sx={{ m: 1, width: '30ch' }}
+              onChange={handleChange('attackUpperBound')}
+            />
 
-          <TextField
-            label="Defense Minimum"
-            type="number"
-            sx={{ m: 1, width: '30ch' }}
-            onChange={handleChange('defenseLowerBound')}
-          />
+            <TextField
+              label="Defense Minimum"
+              type="number"
+              sx={{ m: 1, width: '30ch' }}
+              onChange={handleChange('defenseLowerBound')}
+            />
 
-          <TextField
-            label="Defense Maximum"
-            type="number"
-            sx={{ m: 1, width: '30ch' }}
-            onChange={handleChange('defenseUpperBound')}
-          />
-        </div>
+            <TextField
+              label="Defense Maximum"
+              type="number"
+              sx={{ m: 1, width: '30ch' }}
+              onChange={handleChange('defenseUpperBound')}
+            />
+          </div>
 
-        <div>
-          <TextField
-            label="Sp. Attack Minimum"
-            type="number"
-            sx={{ m: 1, width: '30ch' }}
-            onChange={handleChange('spatkLowerBound')}
-          />
+          <div>
+            <TextField
+              label="Sp. Attack Minimum"
+              type="number"
+              sx={{ m: 1, width: '30ch' }}
+              onChange={handleChange('spatkLowerBound')}
+            />
 
-          <TextField
-            label="Sp. Attack Maximum"
-            type="number"
-            sx={{ m: 1, width: '30ch' }}
-            onChange={handleChange('spatkUpperBound')}
-          />
+            <TextField
+              label="Sp. Attack Maximum"
+              type="number"
+              sx={{ m: 1, width: '30ch' }}
+              onChange={handleChange('spatkUpperBound')}
+            />
 
-          <TextField
-            label="Sp. Defense Minimum"
-            type="number"
-            sx={{ m: 1, width: '30ch' }}
-            onChange={handleChange('spdefLowerBound')}
-          />
+            <TextField
+              label="Sp. Defense Minimum"
+              type="number"
+              sx={{ m: 1, width: '30ch' }}
+              onChange={handleChange('spdefLowerBound')}
+            />
 
-          <TextField label="Sp. Defense Maximum"
-            type="number"
-            sx={{ m: 1, width: '30ch' }}
-            onChange={handleChange('spdefUpperBound')}
-          />
-        </div>
+            <TextField label="Sp. Defense Maximum"
+              type="number"
+              sx={{ m: 1, width: '30ch' }}
+              onChange={handleChange('spdefUpperBound')}
+            />
+          </div>
 
-        <div>
-          <TextField
-            label="Speed Minimum"
-            type="number"
-            sx={{ m: 1, width: '30ch' }}
-            onChange={handleChange('speedLowerBound')}
-          />
+          <div>
+            <TextField
+              label="Speed Minimum"
+              type="number"
+              sx={{ m: 1, width: '30ch' }}
+              onChange={handleChange('speedLowerBound')}
+            />
 
-          <TextField
-            label="Speed Maximum"
-            type="number"
-            sx={{ m: 1, width: '30ch' }}
-            onChange={handleChange('speedUpperBound')}
-          />
+            <TextField
+              label="Speed Maximum"
+              type="number"
+              sx={{ m: 1, width: '30ch' }}
+              onChange={handleChange('speedUpperBound')}
+            />
 
-          <TextField
-            label="Index Minimum"
-            type="number"
-            sx={{ m: 1, width: '30ch' }}
-            onChange={handleChange('indexLowerBound')}
-          />
+            <TextField
+              label="Index Minimum"
+              type="number"
+              sx={{ m: 1, width: '30ch' }}
+              onChange={handleChange('indexLowerBound')}
+            />
 
-          <TextField
-            label="Index Maximum"
-            type="number"
-            sx={{ m: 1, width: '30ch' }}
-            onChange={handleChange('indexUpperBound')}
-          />
-        </div>
+            <TextField
+              label="Index Maximum"
+              type="number"
+              sx={{ m: 1, width: '30ch' }}
+              onChange={handleChange('indexUpperBound')}
+            />
+          </div>
 
-        <Button type="submit">Search</Button>
+          <Button type="submit">Search</Button>
 
-      </form>
-    </Grid>
+        </form>
+      </Grid>
+    </Paper>
   );
 }
